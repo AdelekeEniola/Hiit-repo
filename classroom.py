9d301e6831147ed8db25eceea67488bb75d59b40
AdelekeEniola <demaffairs@gmail.com>
2026-07-24 19:47:08 -0900

    Remove all files previously uploaded

--- classroom.py
+++
@@ -1,4 +0,0 @@
-number=5
-numbertwo=7
-Adittion=number+numbertwo
-print=Adittion
--- practice.java
+++
@@ -1,8 +0,0 @@
-public class Main
-public static void main (string []args){
-int i=0
-while (i<5){
-    system.out.println (i);
-    i++;
-}
-}
